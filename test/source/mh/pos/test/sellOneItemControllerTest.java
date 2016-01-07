488a04dfcbd5aba901e114b453b17da3db328046
= <=>
2016-01-07 16:57:17 +0000

    added libraries

---
+++ test/source/mh/pos/test/sellOneItemControllerTest.java
@@ -0,0 +1,16 @@
+package mh.pos.test;
+
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class sellOneItemControllerTest {
+
+    @Test
+    public void productFound() throws Exception {
+
+
+
+    }
+}
+
