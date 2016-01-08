082068e5a17b811f93077a915237de058e3b2a23
= <=>
2016-01-08 16:24:29 +0000

    Confirmed that in memory catalog respects symantics of catalog

--- test/source/mh/pos/test/FindPriceInMemoryCataglogTest.java
+++ test/source/mh/pos/test/FindPriceInMemoryCataglogTest.java
@@ -11,14 +11,21 @@ public class FindPriceInMemoryCataglogTest {
     @Test
     public void productFound() throws Exception {
         Price foundPrice = Price.cents(1250);
-        Catalog catalog =  new InMemoryCatalog(Collections.singletonMap("12345", foundPrice));
+        Catalog catalog = catalogWith("12345", foundPrice);
         Assert.assertEquals(foundPrice  , catalog.findPrice("12345"));
     }
-
     @Test
     public void productNotFound() throws Exception {
-        Catalog catalog = new InMemoryCatalog(Collections.emptyMap());
+        Catalog catalog = catalogWithout("12345");
         Assert.assertEquals(null, catalog.findPrice("12345"));
+    }
+
+    private InMemoryCatalog catalogWithout(String barcodeToAvoid) {
+        return new InMemoryCatalog(Collections.emptyMap());
+    }
+
+    private InMemoryCatalog catalogWith(String barcode, Price price) {
+        return new InMemoryCatalog(Collections.singletonMap(barcode, price));
     }
 
     public static class InMemoryCatalog implements Catalog {
